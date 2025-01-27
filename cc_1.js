9d2675e31fd24528ac66e65245a1933743a7c462
Isabelalx <isabelalopesxavier@usf.edu>
2025-01-26 21:04:31 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// task 1
+let employeeName = "name_of_employee";
+const employeeId = "id_of_employee";
+var isActive = true;
+
+console.log(typeof employeeName);
+console.log(typeof employeeId);
+console.log(typeof isActive); 
